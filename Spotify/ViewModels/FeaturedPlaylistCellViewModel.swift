9a866afc64f96c6fcc86e0911d77831113d14c6a
coby <coby5502@gmail.com>
2021-11-07 03:50:35 +0900

    set Browse Data

---
+++ Spotify/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  Spotify
+//
+//  Created by Coby Kim on 2021/11/07.
+//
+
+import Foundation
+
+struct FeaturedPlaylistCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let creatorName: String
+}
---
+++ Spotify/ViewModels/RecommendedTracksCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedTracksCellViewModel.swift
+//  Spotify
+//
+//  Created by Coby Kim on 2021/11/07.
+//
+
+import Foundation
+
+struct RecommendedTracksCellViewModel {
+    let name: String
+    let artistName: String
+    let artworkURL: URL?
+}
